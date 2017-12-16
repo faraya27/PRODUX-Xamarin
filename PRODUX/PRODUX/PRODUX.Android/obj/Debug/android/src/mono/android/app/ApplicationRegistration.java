e3a2fe6e5f3d618bfb5c8adc8f768533f5d2717a
faraya27 <f_araya27@hotmail.com>
2017-12-16 10:27:29 -0600

    Cambios no significativos

--- PRODUX/PRODUX/PRODUX.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
+++ PRODUX/PRODUX/PRODUX.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -5,6 +5,7 @@ public class ApplicationRegistration {
 	public static void registerApplications ()
 	{
 				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("PRODUX.Droid.MainApplication, PRODUX.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md560de8d6b2b9c7f1ee85128a9b8782e25.MainApplication.class, md560de8d6b2b9c7f1ee85128a9b8782e25.MainApplication.__md_methods);
 		
 	}
 }
