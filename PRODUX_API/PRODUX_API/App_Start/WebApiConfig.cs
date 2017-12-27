e479bd89224f4de2a54cecdfbbad2a8190c7b607
faraya27 <f_araya27@hotmail.com>
2017-12-26 19:31:35 -0600

    Segundo commit

--- PRODUX_API/PRODUX_API/App_Start/WebApiConfig.cs
+++ PRODUX_API/PRODUX_API/App_Start/WebApiConfig.cs
@@ -16,9 +16,10 @@ namespace PRODUX_API
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            
         }
     }
 }
